d4ed8351ee55572a810b2febae062e748f9185c7
Oliver Liu <oliver.liu@stonybrook.edu>
2023-08-20 15:20:18 -0500

    Add players model and table

--- hello/models.py
+++ hello/models.py
@@ -5,3 +5,13 @@ from django.db import models
 
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
+
+
+class Player(models.Model):
+    last_name = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=200)
+    utr = models.FloatField(default=0.00)
+    info = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.last_name
